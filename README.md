646a302fc36040bb84d85b02967cad14af7541a1
David Martin <home@sirrio.de>
2019-06-11 01:39:47 +0200

    Upload version 1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# WAVdio
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.2.
+
+## Development server
+
+Run `npm start` for a dev server. Navigate to `http://localhost:3000/`.
+
+## Running mocha unit tests
+
+Run `npm run test --exit` to execute the backend unit tests via [Mocha](https://mochajs.org/api/mocha.runner).
+
+## Running karma unit tests
+
+Run `npm run ng test` to execute the frontend unit tests via [Karma](https://karma-runner.github.io).
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component, Inject, LOCALE_ID, OnInit, Renderer2} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) doc: Document, @Inject(LOCALE_ID) locale: string, renderer: Renderer2) {
+    renderer.setAttribute(doc.documentElement, 'lang', locale);
+  }
+
+  ngOnInit(): void {
+  }
+}
